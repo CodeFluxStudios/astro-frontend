be89b857a7d6777766ebc027d419d39ff4bf5d70
Jonas <contact@atalantus.de>
2018-10-23 17:10:22 +0200

    Update src/app/services/account.service.spec.ts

--- src/app/services/account.service.spec.ts
+++ src/app/services/account.service.spec.ts
@@ -2,10 +2,14 @@ import {TestBed} from '@angular/core/testing';
 
 import {AccountService} from './account.service';
 import {HttpClientModule} from '@angular/common/http';
+import {RouterTestingModule} from '@angular/router/testing';
 
 describe('AccountService', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [HttpClientModule]
+    imports: [
+      HttpClientModule,
+      RouterTestingModule
+    ]
   }));
 
   it('should be created', () => {
