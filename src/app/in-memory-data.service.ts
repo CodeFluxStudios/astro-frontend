3544e34d3a40f476a8ffc6809e0c1163881258a5
Jonas <contact@atalantus.de>
2018-10-24 20:51:26 +0200

    Fixed in-memory-web-api

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -1,12 +1,27 @@
-import {InMemoryDbService} from 'angular-in-memory-web-api';
+import {InMemoryDbService, ParsedRequestUrl, RequestInfoUtilities} from 'angular-in-memory-web-api';
 
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
+    const adminGuilds = [
+      {name: 'Sample Guild A', icon: 'iconHash', id: '0'},
+      {name: 'Sample Guild B', icon: 'iconHash', id: '1'},
+      {name: 'Sample Guild C', icon: 'iconHash', id: '2'},
+      {name: 'Sample Guild D', icon: 'iconHash', id: '3'},
+      {name: 'CodeFlux Studios', icon: '8fd9254891141b735c4897c72a74127a', id: '369857813465268227'},
+    ];
 
-    const projects = [];
+    return {adminGuilds};
+  }
 
-    const commands = [];
+  parseRequestUrl(url: string, requestInfoUtils: RequestInfoUtilities): ParsedRequestUrl {
+    let newUrl = '';
 
-    return {projects, commands};
+    switch (url) {
+      case 'api/guilds/admin':
+        newUrl = 'api/adminGuilds';
+        break;
+    }
+
+    return requestInfoUtils.parseRequestUrl(newUrl);
   }
 }
