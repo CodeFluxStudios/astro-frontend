b6abe473e802e7bf7788f1c5f360783f367c11d9
Jonas <contact@atalantus.de>
2018-10-27 23:31:08 +0200

    Changed some assets

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -3,14 +3,16 @@ import {InMemoryDbService, ParsedRequestUrl, RequestInfoUtilities} from 'angular
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const adminGuilds = [
-      {name: 'Sample Guild A', icon: 'iconHash', hasBot: 'false', id: '0'},
-      {name: 'Sample Guild B', icon: 'iconHash', hasBot: 'true', id: '1'},
-      {name: 'Sample Guild C', icon: 'iconHash', hasBot: 'true', id: '2'},
-      {name: 'Sample Guild D', icon: 'iconHash', hasBot: 'true', id: '3'},
-      {name: 'CodeFlux Studios', icon: '8fd9254891141b735c4897c72a74127a', hasBot: 'true', id: '369857813465268227'},
+      {name: 'Test Guild A', icon: 'iconHash', bot_joined: 'true', id: '0'},
+      {name: 'Test Guild B', icon: 'iconHash', bot_joined: 'true', id: '1'},
+      {name: 'Test Guild C', icon: 'iconHash', bot_joined: 'true', id: '2'},
+      {name: 'Test Guild D', icon: 'iconHash', bot_joined: 'true', id: '3'},
+      {name: 'CodeFlux Studios', icon: '8fd9254891141b735c4897c72a74127a', bot_joined: 'true', id: '369857813465268227'},
     ];
 
-    return {adminGuilds};
+    const user = {username: 'TestUser', avatar: '1234234', id: '1234234'};
+
+    return {adminGuilds, user};
   }
 
   parseRequestUrl(url: string, requestInfoUtils: RequestInfoUtilities): ParsedRequestUrl {
@@ -18,6 +20,8 @@ export class InMemoryDataService implements InMemoryDbService {
 
     if (url === 'api/user/guilds/admin') {
       newUrl = 'api/adminGuilds';
+    } else if (url === 'api/user') {
+      newUrl = 'api/user';
     }
 
     return requestInfoUtils.parseRequestUrl(newUrl);
