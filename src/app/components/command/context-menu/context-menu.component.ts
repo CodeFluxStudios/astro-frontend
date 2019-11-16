1f48b952d480bd0f64a7f1c5fda2988ab72e24a4
Jonas <contact@atalantus.de>
2019-11-16 15:10:10 +0100

    Added SearchForNode context to custom context menu

--- src/app/components/command/context-menu/context-menu.component.ts
+++ src/app/components/command/context-menu/context-menu.component.ts
@@ -1,23 +1,44 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-context-menu',
   templateUrl: './context-menu.component.html',
-  styleUrls: ['./context-menu.component.scss', './context-menu.component.theme.scss']
+  styleUrls: ['./context-menu.component.scss', './context-menu.component.theme.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class ContextMenuComponent implements OnInit {
-  @ViewChild('contextMenu', {static: false}) contextMenu;
+export class ContextMenuComponent implements AfterViewInit {
+  @ViewChild('contextMenu', {static: false}) contextMenu: ElementRef;
+  isCreateNodeContext = false;
+  lastMousePos = {x: 0, y: 0};
+  nodeSearchTerm: String;
+
+  nodes = [
+    'Node A',
+    'Node B',
+    'Node C',
+    'Node D',
+    'Node E',
+    'Node F'
+  ];
 
   constructor() {
     this.addContextMenu();
   }
 
-  ngOnInit() {
+  ngAfterViewInit(): void {
+    this.contextMenu.nativeElement.addEventListener('click', (e: MouseEvent) => {
+      console.log('contextMenu click');
+
+      e.preventDefault();
+      e.stopPropagation();
+    });
   }
 
   addContextMenu() {
-    document.addEventListener('contextmenu', (e) => {
+    document.addEventListener('contextmenu', (e: MouseEvent) => {
       e.preventDefault();
+
+      console.log('document contextmenu event click');
 
       // @ts-ignore
       if (e.target.id === 'canvas') {
@@ -28,19 +49,36 @@ export class ContextMenuComponent implements OnInit {
     }, false);
 
     document.addEventListener('click', (e) => {
+      console.log('document click');
+
       this.toggleCustomContextMenu(e, false);
     });
   }
 
+  showCreateNodeContext() {
+    this.isCreateNodeContext = true;
+
+    if (!this.contextMenu.nativeElement.classList.contains('create-node-context')) {
+      this.contextMenu.nativeElement.classList.add('create-node-context');
+    }
+
+    this.moveContextMenu(null, 225, 250);
+  }
+
   toggleCustomContextMenu(e, on: boolean) {
+    this.isCreateNodeContext = false;
+    if (this.contextMenu.nativeElement.classList.contains('create-node-context')) {
+      this.contextMenu.nativeElement.classList.remove('create-node-context');
+    }
+
     if (on) {
       if (!this.contextMenu.nativeElement.classList.contains('open')) {
         this.contextMenu.nativeElement.classList.add('open');
       }
 
-      const origin = this.getContextOrigin(e.pageX, e.pageY - 64);
-      this.contextMenu.nativeElement.style.left = `${origin.x}px`;
-      this.contextMenu.nativeElement.style.top = `${origin.y}px`;
+      this.lastMousePos = {x: e.pageX, y: e.pageY};
+
+      this.moveContextMenu(e, 150, 108);
     } else {
       if (this.contextMenu.nativeElement.classList.contains('open')) {
         this.contextMenu.nativeElement.classList.remove('open');
@@ -48,13 +86,20 @@ export class ContextMenuComponent implements OnInit {
     }
   }
 
-  getContextOrigin(leftOffset, topOffset) {
-    if (leftOffset + 150 > window.innerWidth) {
-      leftOffset -= 150;
+  moveContextMenu(e: MouseEvent, width: number, height: number) {
+    const origin = e !== null ? this.getContextOrigin(e.pageX, e.pageY - 64, width, height) :
+      this.getContextOrigin(this.lastMousePos.x, this.lastMousePos.y - 64, width, height);
+    this.contextMenu.nativeElement.style.left = `${origin.x}px`;
+    this.contextMenu.nativeElement.style.top = `${origin.y}px`;
+  }
+
+  getContextOrigin(leftOffset, topOffset, width: number, height: number) {
+    if (leftOffset + width > window.innerWidth) {
+      leftOffset -= width;
     }
 
-    if (topOffset + 108 > window.innerHeight - 112) {
-      topOffset -= 108;
+    if (topOffset + height > window.innerHeight - 112) {
+      topOffset -= height;
     }
     return {x: leftOffset, y: topOffset};
   }
